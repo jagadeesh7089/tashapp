b6112dc1e54d10fb5dd3feb68ece18806d3b5cfb
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-12 12:40:12 +0530

    jcdc

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './redux-router/counter ';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hii</h1>
+     <Counter></Counter>
     </div>
   );
 }
--- src/redux-router/counter reducer.js
+++ src/redux-router/counter reducer.js
@@ -1,8 +1,15 @@
-const initialstate={
-    count:0
+const initialState={
+    count:0,
+   
 }
-
-function Counterreducer(){
- return state
-}
-export default Counterreducer
+function counterreducer(state=initialState ,action){
+    if(action.type==="INC"){
+        return {...state,count:state.count+1}
+    }
+    if(action.type==="DEC"){
+        return {...state,count:state.count-1}
+    }
+   
+    return state
+} 
+export default counterreducer  
