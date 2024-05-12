78f59e1b1c20570646277b012a15388678fe5ab3
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-12 14:20:17 +0530

    kkk

--- src/redux -router/todolist reducer.js
+++ src/redux -router/todolist reducer.js
@@ -2,6 +2,9 @@ const initialstate={
     todos:["jagadeesh","Lakshman","Ayyappa","Dinesh"]
 }
 function Todolistreducer(state=initialstate,action){
+    if(action.type==="Addtodo"){
+        return {...state,todos:[...state.todos,action.payload]}
+    }
     return state
 }
 export default Todolistreducer
--- src/redux -router/todolist.jsx
+++ src/redux -router/todolist.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { connect } from "react-redux";
 import Todolistreducer from "./todolist reducer";
 function Todolist(props){
     console.log(props)
+    var [newtodo,setNewtodo]=useState("")
     return(
      <div>
-        <input type="text" /> 
-        <button onClick={()=>{props.dispatch({type:"Addtodo",paylaod:})}}>Add todo</button>
+        <input type="text" onChange={(e)=>{setNewtodo(e.target.value)}} /> 
+        <button onClick={()=>{props.dispatch({type:"Addtodo",paylaod:newtodo})}}>Addtodo</button>
         <div>
             {
               props.Todolistreducer.todos.map(todo=>{
