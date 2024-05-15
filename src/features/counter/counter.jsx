5a7477214a882481a9cfc002db553b4c1044194e
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-15 22:13:23 +0530

    ggg

--- src/features/counter/counter.jsx
+++ src/features/counter/counter.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import { inc,dec,reset } from "./counterSlice";
 function Counter(){
-    var x=useSelector(state=>state)
-    console.log(x)
+    var {count}=useSelector(state=>state.counterReducer)
+    var dispatch=useDispatch()
     return(
-        <div>counter</div>
+        <div>
+            <h1>counter:{count}</h1>
+            <button onClick={()=>{dispatch(inc())}}>Increment</button>
+            <button onClick={()=>{dispatch(dec())}}>Decrement</button>
+            </div>
+
     )
 }
 export default Counter
