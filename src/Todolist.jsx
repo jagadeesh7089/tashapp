9c56c10c13c1c981ab7b349220a7ed32abc717f7
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-16 13:13:20 +0530

    fvb

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -7,8 +7,8 @@ function Todolist(props){
     return (
         <div className="border border-2 border-success p-2">
             <h1>Todolist</h1>
-            <input type="text" onChange={(e)=>{setNewtodo(e.target.value)}} />
-            <button onClick={()=>{props.dispatch({type:"Addtodo",payload:newtodo})}}>ADD todo</button>
+            <input type="text" onChange={(e)=>{setNewtodo(e.target.value)}}/>
+            <button onClick={()=>{props.dispatch({type:"Addtodo",payload:newtodo})}}>ADD todo</button> 
             <ul>
                 {
                     props.Todolistreducer.todos.map((todo)=>{
