0341ba29aac1547e86506211f3999476caaa9f7a
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-21 12:58:07 +0530

    dkf

---
+++ src/redux -router/counter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+function Counter(props){
+   console.log(props)
+    return(
+        <div>
+            <h1>Counter:{props.count}</h1>
+             <button onClick={()=>{props.inc()}}>Increment</button>
+             <button onClick={()=>{props.dec()}}>Decrement</button>
+        </div>
+    )
+}
+export default connect(function(state){
+    return state.Counterreducer
+},function(dispatch){
+    return {
+        inc:()=>{dispatch({type:"INC"})},
+        dec:()=>{dispatch({type:"DEC"})},
+    }
+}) (Counter)
