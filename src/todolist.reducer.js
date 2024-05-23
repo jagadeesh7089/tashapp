8c4a10483f5296dcbdf35b5b2c767198343a0e2b
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-05-23 16:42:19 +0530

    ksks

--- src/todolist.reducer.js
+++ src/todolist.reducer.js
@@ -2,7 +2,7 @@ const initialState={
    
     todos:["jagadeesh","Lakshman","Suribabu","Ayyappa"]
 }
-function  Todolistreducer(state=initialState ,action){
+function  Todolistreducer(state=initialState,action){
     
     if(action.type==="Addtodo"){
       return {...state,todos:[...state.todos,action.payload],}
