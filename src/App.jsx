2792c81861a18b0e0051c753a2929dd5695f236c
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-06-03 16:15:36 +0530

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 function App() {
+  useEffect(()=>{
+    axios.get("")
+  },[])
   return (
-    <div className="App">
+    <div>
       
     </div>
   );
