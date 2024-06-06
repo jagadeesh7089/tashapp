e05d95df1bcde63b7017f9deedfaa8e63078593b
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-06-06 12:43:03 +0530

    kkk

---
+++ src/Accordian/Accordian 1.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Accordian(){
+    function accor(e){
+      
+    }
+
+    return (
+        <div>
+           <div>
+               <h1 onClick={(e)=>{accor(e)}}>A</h1>
+                 <div>
+                 &nbsp;&nbsp;&nbsp;&nbsp;<span>Apple</span>
+                    
+                 </div>
+
+           </div>
+        </div>
+    )
+}
+export default Accordian
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Accordian from './Accordian/Accordian 1';
 
 function App() {
   return (
     <div className="App">
      <h1>Hii</h1>
+     <Accordian></Accordian>
     </div>
   );
 }
