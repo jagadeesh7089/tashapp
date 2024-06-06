498d52a1885926b14d5dc41f12a95a8b49f37627
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-06-06 14:00:02 +0530

    jjj

--- src/Accordian/Accordian 1.jsx
+++ src/Accordian/Accordian 1.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 function Accordian(){
+    var status=false
+    var[selected,setSelected]=useState()
     function accor(e){
+         console.dir(e)
+         setSelected(e.target.id)
+        // if(){
+
+        // }
       
     }
 
     return (
         <div>
            <div>
-               <h1 onClick={(e)=>{accor(e)}}>A</h1>
-                 <div>
+               <h1 onClick={(e)=>{accor(e)}} state={status} id="d1">A</h1>
+                 <div style={{display:"none"}}>
                  &nbsp;&nbsp;&nbsp;&nbsp;<span>Apple</span>
                     
                  </div>
