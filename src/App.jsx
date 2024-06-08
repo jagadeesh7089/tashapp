5d03c8ab97cb35150aa4543766a47b161854a87a
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-06-08 12:50:18 +0530

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useState } from 'react';
-function App() {
-  var [products,setProducts]=useState([])
-  var [selectedprod,setSelectedprod]=useState({id:0,statu:false})
-
+import React from "react";
+import { useEffect } from "react";
+function App(){
   useEffect(()=>{
-    axios.get("https://fakestoreapi.com/products").then(res=>{
-      setProducts(res.data)
-    })
+     axios.get("")
   },[])
-
-  function dispatch(i){
-     setSelectedprod({id:i,statu:!selectedprod.statu})
-  }
-  return (
+  return(
     <div>
-        {
-          products?.map(prod=>{
-           return <div className='m-2 p-3 bg bg-dark text-white'>
-             <div onClick={()=>{dispatch(prod.id)}} ><p>{prod.title} <i class="bi bi-plus-circle"></i></p></div>
-             <div style={(prod.id===selectedprod.id && selectedprod.statu)?{display:"block"}:{display:"none"}}>
-              <h4>Details</h4>
-             </div>
-             </div>
-          })
-        }
+        
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
