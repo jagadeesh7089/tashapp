3f8173de740a9f9212706d517ff1fa79a944849d
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-06-08 13:35:22 +0530

    jjhfc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 import { useEffect } from "react";
 function App(){
+  var [product,setProduct]=useState([])
   useEffect(()=>{
-     axios.get("")
+     axios.get("https://fakestoreapi.com/products").then(res=>{
+        var temp=JSON.parse(JSON.stringify(res.data))
+          temp.map(s=>{
+            return {...s,status:false}
+          })
+          setProduct(temp)
+     })
   },[])
-  return(
-    <div>
+  function expand(id){
+    var temp=JSON.parse(JSON.stringify(product))
+       temp.map((a)=>{
+          if(a.id===id){
+            a.status=!a.status
+          }
+          return a
+        })
         
+        setProduct(temp)
+
+  }
+
+  return(
+    <div>  
+           <div>
+            {
+              product?.map((prod)=>{
+                return <div className="bg bg-warning" onClick={()=>{expand(prod.id)}}> <p>{prod.title}</p> 
+                <div style={prod.status?{display:"block"}:{display:"none"}}>
+                  <p>Details</p>
+                </div>
+                </div>
+              })
+            }
+           </div>
     </div>
   )
 }
