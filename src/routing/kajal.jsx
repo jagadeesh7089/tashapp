d7ac02a223d54dedfb2d14ff1e43756a20c54ee7
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-08-01 11:06:07 +0530

    jjjj

--- src/routing/kajal.jsx
+++ src/routing/kajal.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 function Kajal(){
     return (
         <div>
             <h1>Kazal</h1>
+            
         </div>
     )
 }
