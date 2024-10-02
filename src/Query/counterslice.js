5994703c220ef936df1658610835bd6cc0c681cf
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-10-02 12:57:37 +0530

    todo

---
+++ src/Query/counterslice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState={
+    count:0
+}
+
+export const counterSlice=createSlice({
+    name:'counterslice',
+    initialState,
+    reducers:{
+        inc:(state,action)=>{
+            state.count=state.count+1
+        },
+
+        dec:(state,action)=>{
+
+            state.count=state.count-1
+
+        }
+    }
+
+})
+export const {dec,inc}=counterSlice.actions
+
+
+
+
