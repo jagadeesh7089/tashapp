f5905118efba48bf0457895a6a86e40a9dc0c66e
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-10-12 18:25:01 +0530

    status

--- src/Accordian/Accordian 1.jsx
+++ src/Accordian/Accordian 1.jsx
@@ -3,32 +3,43 @@ import React, { useEffect, useState } from "react";
 function Accor(){
 var [product,setProduct]=useState([])
 var [pid,setPid]=useState(0)
-var [temp,setTemp]=useState([])
+
   useEffect(()=>{
     axios('https://fakestoreapi.com/products').then((res)=>{
       // console.log(res.data)
-      setProduct(res.data)
+      var temp=res.data
+     temp=temp.map((s=>{
+        return {...s,status:false}
+       }))
+
+       setProduct([...temp])
+      
     })
   },[])
 
-   function pcl(e){
-      setPid(e)
-      console.log(pid)
-   }
+  function abc(pro,i){
+    console.log(pro)
+    pro.status=!pro.status
+    console.log(pro)
+    
+  
+
+  }
 
   return (
     <div>
        {
-      setTemp=product?.map((prod,i)=>{
-          return
-
+        product?.map((pro,i)=>{
+          return <div>
+            <div onClick={()=>{abc(pro,i)}}>{pro.title}</div>
+            <div style={pro.status===true?{display:"block"}:{display:"none"}}>
+              <div>{pro.price}</div>
+              <div>{pro.category}</div>
+            </div>
+          </div>
         })
-
+       }
        
-       }
-       {
-        console.log(temp)
-       }
     </div>
   )
 }
