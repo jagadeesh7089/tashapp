b27e284c25905bc98b020b9a944b7f57a706b051
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-10-12 17:28:34 +0530

    acc

--- src/Accordian/Accordian 1.jsx
+++ src/Accordian/Accordian 1.jsx
@@ -1,13 +1,33 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 function Accor(){
-
+var [product,setProduct]=useState([])
+var [pid,setPid]=useState(0)
   useEffect(()=>{
-    axios('')
+    axios('https://fakestoreapi.com/products').then((res)=>{
+      // console.log(res.data)
+      setProduct(res.data)
+    })
   },[])
 
+   function pcl(e){
+      setPid(e)
+      console.log(pid)
+   }
+
   return (
-    <div></div>
+    <div>
+       {
+        product.map((prod,i)=>{
+          return <div>
+            <div onClick={()=>{pcl(prod.id)}}>{prod.title}</div>
+            <div>{prod.id}</div>
+            <div >{prod.price}</div>
+          </div>
+
+        })
+       }
+    </div>
   )
 }
 export default Accor
