1f8e0bb0a5cd4828b3fb292ec0caabec5e9a7e4a
jagadeesh7089 <jagadeeshk775@gmail.com>
2024-10-12 16:59:42 +0530

    accor

--- src/Accordian/Accordian 1.jsx
+++ src/Accordian/Accordian 1.jsx
@@ -1,39 +1,13 @@
-import React, { useState } from "react";
-function Accordian(){
- 
-   function click (e){ 
-        if(!e.status){
-          e.status=!e.status
-          console.dir(e)
-        }
-        else{
-          e.status=!e.status
-          console.log(e.status)
-        }
-      }
-     
-    
+import axios from "axios";
+import React, { useEffect } from "react";
+function Accor(){
 
-  
-    return (
-        <div>
-            <div>
-              <h4 onClick={(e)=>{click({ ...e,status:false})}} id="d1" >A</h4>
-               &nbsp; &nbsp;<p style={{display:"none"}} >Apple</p>
-            </div>
-            <div>
-              <h4 onClick={(e)=>{click({...e,status:false})}} id="d2" >B</h4>
-               &nbsp; &nbsp;<p style={{display:"none"}} >Balloon</p>
-            </div>
-            <div>
-              <h4 onClick={(e)=>{click({...e,status:false})}} id="d3" >C</h4>
-               &nbsp; &nbsp;<p style={{display:"none"}} >Carrot</p>
-            </div>
-            <div>
-              <h4 onClick={(e)=>{click({...e,status:false})}} id="d4" >D</h4>
-               &nbsp; &nbsp;<p style={{display:"none"}} >Danger</p>
-            </div>
-        </div>
-    )
+  useEffect(()=>{
+    axios('')
+  },[])
+
+  return (
+    <div></div>
+  )
 }
-export default Accordian
+export default Accor
